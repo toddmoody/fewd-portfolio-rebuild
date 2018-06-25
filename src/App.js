8f80e8aa6f3ef9316ffd03244cd1e84575f20340
Todd Moody <todd@moodsdigital.co.nz>
2018-06-25 21:55:43 +1200

    background assets

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import './App.scss';
 
 import Header from './components/Header'
-import Intro from './components/Intro'
+
 import Work from './components/Work'
 import Projects from './components/Projects'
 import Contact from './components/Contact'
@@ -13,7 +13,7 @@ class App extends Component {
     return (
       <div className="App">
         <Header />
-        <Intro />
+
         <Work />
         <Projects />
         <Contact />
